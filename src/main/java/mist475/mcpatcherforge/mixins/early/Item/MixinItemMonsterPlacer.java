a258f7c4d1ee3f5142c4fe346fbd38cf3beefc74
Mist475 <dubbelkris18@outlook.com>
2023-10-06 19:56:14 +0200

    migrate ItemMonsterPlacer.getColorFromItemStack overwrite to inject

--- src/main/java/mist475/mcpatcherforge/mixins/early/Item/MixinItemMonsterPlacer.java
+++ src/main/java/mist475/mcpatcherforge/mixins/early/Item/MixinItemMonsterPlacer.java
@@ -1,34 +1,20 @@
 package mist475.mcpatcherforge.mixins.early.Item;
 
-import net.minecraft.entity.EntityList;
 import net.minecraft.item.ItemMonsterPlacer;
 import net.minecraft.item.ItemStack;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.prupe.mcpatcher.cc.ColorizeItem;
-
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 
 @Mixin(ItemMonsterPlacer.class)
 public abstract class MixinItemMonsterPlacer {
 
-    /**
-     * @author Mist475 (adapted from Paul Rupe)
-     * @reason Bound to be incompatible anyway
-     */
-    @SideOnly(Side.CLIENT)
-    @Overwrite
-    public int getColorFromItemStack(ItemStack add, int spots) {
-        final EntityList.EntityEggInfo entityegginfo = EntityList.entityEggs.get(add.getItemDamage());
-        if (entityegginfo == null) {
-            return ColorizeItem.colorizeSpawnerEgg(16777215, add.getItemDamage(), spots);
-        }
-        if (spots == 0) {
-            return ColorizeItem.colorizeSpawnerEgg(entityegginfo.primaryColor, add.getItemDamage(), spots);
-        }
-        return ColorizeItem.colorizeSpawnerEgg(entityegginfo.secondaryColor, add.getItemDamage(), spots);
+    @Inject(method = "getColorFromItemStack(Lnet/minecraft/item/ItemStack;I)I", at = @At("RETURN"), cancellable = true)
+    private void modifyGetColorFromItemStack(ItemStack itemStack, int spots, CallbackInfoReturnable<Integer> cir) {
+        cir.setReturnValue(ColorizeItem.colorizeSpawnerEgg(cir.getReturnValue(), itemStack.getItemDamage(), spots));
     }
 }
