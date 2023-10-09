83b93b63211360ff0833cd50a7dd8680da18fa9d
Mist475 <dubbelkris18@outlook.com>
2023-10-09 13:24:01 +0200

    fix TileEntitySignRenderer crashing out of dev

--- src/main/java/mist475/mcpatcherforge/mixins/early/client/renderer/tileentity/MixinTileEntitySignRenderer.java
+++ src/main/java/mist475/mcpatcherforge/mixins/early/client/renderer/tileentity/MixinTileEntitySignRenderer.java
@@ -4,18 +4,20 @@ import net.minecraft.client.renderer.tileentity.TileEntitySignRenderer;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import com.prupe.mcpatcher.cc.ColorizeWorld;
 
 @Mixin(TileEntitySignRenderer.class)
 public abstract class MixinTileEntitySignRenderer {
 
-    @ModifyVariable(
+    @ModifyArg(
         method = "renderTileEntityAt(Lnet/minecraft/tileentity/TileEntitySign;DDDF)V",
-        at = @At("STORE"),
-        ordinal = 0)
-    private byte modifyRenderTileEntityAt(byte value) {
-        return (byte) ColorizeWorld.colorizeSignText();
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/FontRenderer;drawString(Ljava/lang/String;III)I"),
+        index = 3)
+    private int modifyRenderTileEntityAt(int color) {
+        return ColorizeWorld.colorizeSignText();
     }
 }
