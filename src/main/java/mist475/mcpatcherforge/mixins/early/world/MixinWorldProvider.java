5ec53c8de31ef15df3bff8fe1c41f78219f23194
Mist475 <dubbelkris18@outlook.com>
2023-10-11 11:49:02 +0200

    migrate overwrite in WorldProvider

--- src/main/java/mist475/mcpatcherforge/mixins/early/world/MixinWorldProvider.java
+++ src/main/java/mist475/mcpatcherforge/mixins/early/world/MixinWorldProvider.java
@@ -1,48 +1,53 @@
 package mist475.mcpatcherforge.mixins.early.world;
 
-import net.minecraft.util.MathHelper;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.WorldProvider;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.prupe.mcpatcher.cc.ColorizeWorld;
 import com.prupe.mcpatcher.cc.Colorizer;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-
 @Mixin(WorldProvider.class)
 public abstract class MixinWorldProvider {
 
-    /**
-     * @author Mist475 (adapted from Paul Rupe)
-     * @reason code block wrapped in if
-     */
-    @SideOnly(Side.CLIENT)
-    @Overwrite
-    public Vec3 getFogColor(float p_76562_1_, float p_76562_2_) {
-        float f2 = MathHelper.cos(p_76562_1_ * (float) Math.PI * 2.0F) * 2.0F + 0.5F;
+    @Unique
+    private boolean mcpatcher_forge$computeFogColor;
 
-        if (f2 < 0.0F) {
-            f2 = 0.0F;
-        }
+    @Inject(method = "getFogColor(FF)Lnet/minecraft/util/Vec3;", at = @At("HEAD"))
+    private void modifyGetFogColor1(float p_76562_1_, float p_76562_2_, CallbackInfoReturnable<Vec3> cir) {
+        this.mcpatcher_forge$computeFogColor = ColorizeWorld.computeFogColor((WorldProvider) (Object) this, p_76562_1_);
+    }
 
-        if (f2 > 1.0F) {
-            f2 = 1.0F;
+    @ModifyConstant(method = "getFogColor(FF)Lnet/minecraft/util/Vec3;", constant = @Constant(floatValue = 0.7529412F))
+    private float modifyGetFogColor1(float constant) {
+        if (this.mcpatcher_forge$computeFogColor) {
+            return Colorizer.setColor[0];
         }
+        return constant;
+    }
 
-        float f3 = 0.7529412F;
-        float f4 = 0.84705883F;
-        float f5 = 1.0F;
-        // patch start
-        if (ColorizeWorld.computeFogColor((WorldProvider) (Object) this, p_76562_1_)) {
-            f3 = Colorizer.setColor[0];
-            f4 = Colorizer.setColor[1];
-            f5 = Colorizer.setColor[2];
+    @ModifyConstant(method = "getFogColor(FF)Lnet/minecraft/util/Vec3;", constant = @Constant(floatValue = 0.84705883F))
+    private float modifyGetFogColor2(float constant) {
+        if (this.mcpatcher_forge$computeFogColor) {
+            return Colorizer.setColor[1];
         }
-        return Vec3.createVectorHelper(f3 * (f2 * 0.94f + 0.06f), f4 * (f2 * 0.94f + 0.06f), f5 * (f2 * 0.91f + 0.09f));
-        // patch end
+        return constant;
+    }
+
+    @ModifyConstant(
+        method = "getFogColor(FF)Lnet/minecraft/util/Vec3;",
+        constant = @Constant(floatValue = 1.0F, ordinal = 2))
+    private float modifyGetFogColor3(float constant) {
+        if (this.mcpatcher_forge$computeFogColor) {
+            return Colorizer.setColor[2];
+        }
+        return constant;
     }
 }
